5233db1f824c546244acdc3ecb04a588f3ddab40
Sintya Fadillah Nurrahman <sintyafadillah7171@gmail.com>
2023-06-18 23:58:27 +0700

    menambahkan tugas

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Sintya Fadillah Nurrahman" // TODO: replace this
+const studentId = "FE5106875" // TODO: replace this
+
+export { url, studentName, studentId }
